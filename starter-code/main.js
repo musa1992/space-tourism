3ebe160e1af809214d7df7b4b4e80974a7e55869
musa1992 <mosesbwire91@gmail.com>
2022-01-15 18:19:06 +0300

    enable mobile slide navigation menu

---
+++ starter-code/main.js
@@ -0,0 +1,13 @@
+let menuIcon = document.getElementById('mobile-menu');
+let mobileMenu = document.getElementById('navigation')
+let closeMenuBtn = document.getElementById('close-menu')
+
+
+menuIcon.addEventListener('click', ()=> {
+    mobileMenu.style.right = "0"
+    
+})
+
+closeMenuBtn.addEventListener('click', ()=>{
+    mobileMenu.style.right = "-60%"
+})
