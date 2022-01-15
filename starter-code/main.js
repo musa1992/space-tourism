43024f8146bec1d1b46c2b2f89c1ea08ed492e9c
musa1992 <mosesbwire91@gmail.com>
2022-01-15 20:17:21 +0300

    add display none to hambuger menu when clicked and display block when menu is closed

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,9 +5,11 @@ let closeMenuBtn = document.getElementById('close-menu')
 
 menuIcon.addEventListener('click', ()=> {
     mobileMenu.style.right = "0"
+    menuIcon.style.display = "none"
     
 })
 
 closeMenuBtn.addEventListener('click', ()=>{
     mobileMenu.style.right = "-70%"
+    menuIcon.style.display  = "block"
 })
