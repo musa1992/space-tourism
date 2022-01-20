41479138a6d6a82fc6ca104af874ed3475221054
musa1992 <mosesbwire91@gmail.com>
2022-01-20 21:59:20 +0300

    add html structure to mars moon and titan html pages

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,6 +2,8 @@ let menuIcon = document.getElementById('mobile-menu');
 let mobileMenu = document.getElementById('navigation')
 let closeMenuBtn = document.getElementById('close-menu')
 
+let techNav = document.querySelectorAll('.nav-tech')
+
 
 menuIcon.addEventListener('click', ()=> {
     mobileMenu.style.right = "0"
@@ -12,4 +14,12 @@ menuIcon.addEventListener('click', ()=> {
 closeMenuBtn.addEventListener('click', ()=>{
     mobileMenu.style.right = "-70%"
     menuIcon.style.display  = "block"
+})
+
+
+techNav.forEach(element =>{
+    element.addEventListener('click', (e)=>{
+        e.target.style.backgroundColor = "#FFFFFF"
+        
+    })
 })
